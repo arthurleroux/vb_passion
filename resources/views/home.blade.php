d9973702bfe998633dc3c1a7c31b52a31c1aa83a
ArnaudDoubremelle <a-r-n-a-u-d-@hotmail.fr>
2019-02-19 10:56:15 +0100

    Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,82 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="text-center">
-        <h1>Home</h1>
+    <div id="carouselExampleControls" class="carousel slide slider-home-header" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleControls" data-slide-to="1"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item slide-voiture-1 active">
+                <div class="slide-center container">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <img src="//res.cloudinary.com/hrscywv4p/image/upload/c_limit,fl_lossy,h_1440,w_720,f_auto,q_auto/v1/1501348/Sans_titre_wxvdsh.png" alt="" title="" class="crop-default" data-description="">
+                        </div>
+                        <div class="col-lg-6">
+                            <h1>Le Rallye des Anglaises</h1>
+                            <h4 class="s-component-content s-font-heading">
+                                <ul>
+                                    <li style="text-align: justify; font-size: 100%;">Voitures de marque anglaises</li>
+                                    <li style="font-size: 100%; text-align: justify;">Départ du Touquet</li>
+                                    <li style="text-align: justify; font-size: 100%;">Art de vivre à la française ...</li>
+                                </ul>
+                                <div>&nbsp;</div>
+                                <div>&nbsp;</div>
+                                <div>&nbsp;</div>
+                                <div>&nbsp;</div>
+                            </h4>
+                            <a href="http://www.vbpassion.com/le-rallye-des-anglaises" data-component="button" target="_self" class="btn btn-primary">Inscription</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item slide-voiture-2">
+                <div class="slide-center container">
+                    <div class="row">
+                        <div class="col-lg-12 text-center">
+                            <h1>Rallyes Evénements</h1>
+                            <h4>Collection & Prestige</h4>
+                            <a href="http://www.vbpassion.com/le-rallye-des-anglaises" data-component="button" target="_self" class="btn btn-primary">Inscription</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        <div class="s-first-section-arrow css-1yk3s2f"><div class="shape1"></div><div class="shape2"></div></div>
     </div>
+<div class="container">
+    <section id="welcome" class="section-content text-center">
+        <h2>
+            Bienvenue chez VB Passion
+        </h2>
+        <h4>
+            Vivez et partagez votre passion !
+        </h4>
+        <p>Découvrez VB PASSION, expert en rallye de voitures anciennes. VB Passion est spécialisé dans la création de rallyes de voitures anciennes et de tous types d'événements liés à l'automobile, rallye de régularité, rallye promenade, roulage circuit. Notre agence, pour la préparation de rallye de voitures anciennes est situé en région parisienne, aux portes de Paris. Nous organisons des rallyes de voitures anciennes dans la France entière.</p>
+        <p class="welcome-p-last">Contactez nous pour en apprendre d'avantage.</p>
+        <div class="row">
+            <div class="col-lg-4">
+                <img src="/img/img-voiture1.webp" alt="img voiture 1" class="img-fluid">
+                <p><strong>Le prochain rendez vous à ne pas manquer.</strong></p>
+            </div>
+            <div class="col-lg-4">
+                <img src="/img/img-voiture2.webp" alt="img voiture 2" class="img-fluid">
+                <p><strong>Rallye à la carte, Un rallye à votre image ! </strong></p>
+            </div>
+            <div class="col-lg-4">
+                <img src="/img/img-voiture3.webp" alt="img voiture 3" class="img-fluid">
+                <p><strong>Contact</strong></p>
+            </div>
+        </div>
+    </section>
+</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,11 +7,12 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'VB PASSION') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('plugins/slick/slick-theme.css') }}">
@@ -19,102 +20,138 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src=" {{ asset('img/logo.png') }}" alt="">
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                <img src=" {{ asset('img/logo.png') }}" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
 
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Rallyes
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="nav-link" href="{{ route('rallye_anglaises') }}">Le rallye des anglaises</a>
+                            <a class="nav-link" href="{{ route('rallye_carte') }}">Rallye à la carte</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('evenements') }}">Evenements</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('partenaires') }}">Partenaires</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('galerie') }}">Galerie</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('contact') }}">Contact</a>
+                    </li>
+                    @auth
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Rallyes
+                                Location
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="nav-link" href="{{ route('rallye_anglaises') }}">Le rallye des anglaises</a>
-                                <a class="nav-link" href="{{ route('rallye_carte') }}">Rallye à la carte</a>
+                                <a class="nav-link" href="{{ route('car.create') }}">Mettre une voiture à louer</a>
+                                <a class="nav-link" href="{{ route('show_owned_cars') }}">Mes voitures en location</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="nav-link" href="{{ route('car.index') }}">Louer une voiture</a>
                             </div>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('evenements') }}">Evenements</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('partenaires') }}">Partenaires</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('galerie') }}">Galerie</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('contact') }}">Contact</a>
-                        </li>
-                        @auth
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Location
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="nav-link" href="{{ route('car.create') }}">Mettre une voiture à louer</a>
-                                    <a class="nav-link" href="{{ route('show_owned_cars') }}">Mes voitures en location</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="nav-link" href="{{ route('car.index') }}">Louer une voiture</a>
-                                </div>
-                            </li>
-                        @endauth
-                    </ul>
+                    @endauth
+                </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('profile.show', Auth::id()) }}">Mon profil</a>
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('profile.show', Auth::id()) }}">Mon profil</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-2">
+                            <img alt="logo footer" class="img-fluid" width="300" height="125" src="//res.cloudinary.com/hrscywv4p/image/upload/c_limit,fl_lossy,h_300,w_300,f_auto,q_auto/v1/1501348/vbp_Logo_o1g4y9.png" data-src="//res.cloudinary.com/hrscywv4p/image/upload/c_limit,fl_lossy,h_300,w_300,f_auto,q_auto/v1/1501348/vbp_Logo_o1g4y9.png">
+                        </div>
+                        <div class="col-lg-2 align-self-end">
+                            <p><b>Pour nous contacter</b></p>
+                        </div>
+                        <div class="col-lg-2 align-self-end">
+                            <p>Mail :</p>
+                            <p>info@<wbr>vbpassion.com</p>
+                        </div>
+                        <div class="col-lg-2 align-self-end">
+                            <p>Téléphone :</p>
+                            <p>07.77.31.12.21</p>
+                        </div>
+                        <div class="col-lg-2 offset-lg-2 align-self-end">
+                            <a class="s-social-icon s-social-icon-facebook" href="https://www.facebook.com/VBPassionAuto" target="_blank"><span class="fa fa-facebook"></span></a>
+                            <a class="s-social-icon s-social-icon-linkedin" href="https://www.linkedin.com/company/vbpassion/" target="_blank"><span class="fa fa-linkedin"></span></a>
+                            <a class="s-social-icon s-social-icon-instagram" href="https://www.instagram.com/vbpassionauto/" target="_blank"><span class="fa fa-instagram"></span></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <hr>
+                    <p>
+                        <span class="s-text-color-black">VB PASSION © 2018 </span><a href="https://uploads.strikinglycdn.com/files/b37002ac-75fa-42ba-94fc-4d96b142ce59/Mentions légales Sites.pdf"><span class="s-text-color-custom1">Mentions Légales</span></a>
+                    </p>
                 </div>
             </div>
-        </nav>
+        </div>
+    </footer>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+</div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script src="{{ asset('plugins/slick/slick.js') }}"></script>
-    <script src="{{ asset('js/main.js') }}"></script>
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('plugins/slick/slick.js') }}"></script>
+<script src="{{ asset('js/main.js') }}"></script>
 
 </body>
 </html>
